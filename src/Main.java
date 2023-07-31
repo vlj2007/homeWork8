60fe21f5ccbd1f0adfb428392b90ee292d4472c0
vlj2007 <vlj2007@yandex.ru>
2023-07-31 21:10:22 +0700

    I_have_finished_task3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-
+        task3();
+        task4();
     }
 
     public static void task1(){
@@ -12,29 +13,54 @@ public class Main {
         int [ ] carNumbers = {1347, 1800, 1009, 2900, 2751, 4422, 1223, 5524, 8115};
     }
 
-    public static void task2(){
+    public static void task2() {
         System.out.println("Задача 2");
-        int [] weight = new int [2];
-        double [] weights = {1.57, 7.654, 9.986};
-        int [ ] carNumbers = {1347, 1800, 1009, 2900, 2751, 4422, 1223, 5524, 8115};
+        int[] weight = new int[2];
+        double[] weights = {1.57, 7.654, 9.986};
+        int[] carNumbers = {1347, 1800, 1009, 2900, 2751, 4422, 1223, 5524, 8115};
 
-        System.out.println("Массив weight");
-        for(int i=0; i<=1 ; i++){
-            System.out.println("Элемент " + i +  " значение "+ weight[i]);
+
+        for (int i = 0; i <= 1; i++) {
+            System.out.print(weight[i] + " ");
         }
 
-        System.out.println("Массив weights");
-        for(int i=0; i<=2 ; i++){
-            System.out.println("Элемент " + i +  " значение "+ weights[i]);
+        System.out.println();
+        for (int i = 0; i <= 2; i++) {
+            System.out.print(weights[i] + " ");
         }
 
-        System.out.println("Массив carNumbers");
-        for(int i=0; i<=8 ; i++){
-            System.out.println("Элемент " + i +  " значение "+ carNumbers[i]);
+        System.out.println();
+        for (int i = 0; i <= 8; i++) {
+            System.out.print(carNumbers[i] + " ");
         }
+        System.out.println();
+    }
+
+
+        public static void task3(){
+            System.out.println("Задача 3");
+            int [] weight = new int [2];
+            double [] weights = {1.57, 7.654, 9.986};
+            int [ ] carNumbers = {1347, 1800, 1009, 2900, 2751, 4422, 1223, 5524, 8115};
+
+            for(int i=1; i>=0 ; i = i-1){
+                System.out.print(weight[i] + " ");
+            }
+
+            System.out.println();
+            for(int i=2; i>=0 ; i = i-1){
+                System.out.print(weights[i] + " ");
+            }
+
+            System.out.println();
+            for(int i=8; i>=0 ; i = i-1){
+                System.out.print(carNumbers[i] + " ");
+            }
+    }
+
+    public static void task4(){
 
 
     }
-
 
 }
