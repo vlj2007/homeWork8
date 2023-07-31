c3bff521cafc60edfffc369cee11693a9511ef8a
vlj2007 <vlj2007@yandex.ru>
2023-07-31 20:20:36 +0700

    I_have_finished_task1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
     public static void task1(){
         System.out.println("Задача 1");
-
-
+        int [] weight = new int [2];
+        double [] weights = {1.57, 7.654, 9.986};
+        int [ ] vacationJuly = {17, 18, 19, 20, 21, 22, 23, 24, 25};
     }
+
+
 }
