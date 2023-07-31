f1c4d3deff17eb74d63c20717d0ac4a7198eb250
vlj2007 <vlj2007@yandex.ru>
2023-07-31 20:44:39 +0700

    I_have_finished_task2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+
     }
 
     public static void task1(){
         System.out.println("Задача 1");
         int [] weight = new int [2];
         double [] weights = {1.57, 7.654, 9.986};
-        int [ ] vacationJuly = {17, 18, 19, 20, 21, 22, 23, 24, 25};
+        int [ ] carNumbers = {1347, 1800, 1009, 2900, 2751, 4422, 1223, 5524, 8115};
+    }
+
+    public static void task2(){
+        System.out.println("Задача 2");
+        int [] weight = new int [2];
+        double [] weights = {1.57, 7.654, 9.986};
+        int [ ] carNumbers = {1347, 1800, 1009, 2900, 2751, 4422, 1223, 5524, 8115};
+
+        System.out.println("Массив weight");
+        for(int i=0; i<=1 ; i++){
+            System.out.println("Элемент " + i +  " значение "+ weight[i]);
+        }
+
+        System.out.println("Массив weights");
+        for(int i=0; i<=2 ; i++){
+            System.out.println("Элемент " + i +  " значение "+ weights[i]);
+        }
+
+        System.out.println("Массив carNumbers");
+        for(int i=0; i<=8 ; i++){
+            System.out.println("Элемент " + i +  " значение "+ carNumbers[i]);
+        }
+
+
     }
 
 
